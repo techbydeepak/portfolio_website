5acae2b273b1a8aea45e64c64307e92f6f33a11b
techbydeepak <deepakrathour05tech@gmail.com>
2025-06-06 21:07:07 +0530

    first commit update package

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio-Website/',
+  // base: '/Portfolio-Website/',
   plugins: [react(),
     tailwindcss(),
   ],
